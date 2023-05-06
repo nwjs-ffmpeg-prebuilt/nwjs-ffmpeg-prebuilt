b105dadfefed06ae247237bd5e011a430c5e7207
Ayushman Chhabra <ayushmxn@outlook.com>
2023-05-06 15:27:32 +0530

    feat(ci): add daily cron job to check latest version
    If the NW.js version is greater than FFmpeg's latest release's version, run build

---
+++ .github/workflows/is_latest.sh
@@ -0,0 +1,13 @@
+# Store versions in variables
+NW=$(curl -s https://nwjs.io/versions | jq -r ".latest");
+FF=v$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases | jq -r ".[0].tag_name");
+
+NW=${NW:1};
+FF=${FF:1};
+
+# If NW.js version is greater than FFmpeg version, exit with error
+if [ $NW -gt $FF ]; then
+    exit 1;
+else 
+    exit 0;
+fi
