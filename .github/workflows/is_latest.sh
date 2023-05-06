1bfe83f6bc5a553fb62f635b93ad667bd90455c2
Ayushman Chhabra <ayushmxn@outlook.com>
2023-05-06 16:07:10 +0530

    ci: use `bc`

--- .github/workflows/is_latest.sh
+++ .github/workflows/is_latest.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Store versions in variables
-NW=$(curl -s https://nwjs.io/versions | jq -r ".latest");
-FF=v$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases | jq -r ".[0].tag_name");
+sudo apt update
+sudo apt install -y jq bc
 
-NW=${NW:1};
-FF=${FF:1};
+# Store versions in variables
+NW=$(curl -s https://nwjs.io/versions | jq -r ".latest")
+FF=v$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases | jq -r ".[0].tag_name")
+
+NW=${NW:1}
+FF=${FF:1}
 
 # If NW.js version is greater than FFmpeg version, exit with error
-if [ $NW -gt $FF ]; then
+FLAG=$(echo "$NW > $FF" | bc -l)
+
+if [ $FLAG == 0 ]; then
     exit 1;
-else 
+else
     exit 0;
 fi
