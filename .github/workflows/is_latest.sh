f3354321041decd7391290750c33ffdff24cd0db
Ayushman Chhabra <ayushmxn@outlook.com>
2023-05-06 16:40:27 +0530

    fix(ci): compare versions

--- .github/workflows/is_latest.sh
+++ .github/workflows/is_latest.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y jq bc
-
 # Store versions in variables
 NW=$(curl -s https://nwjs.io/versions | jq -r ".latest")
 FF=v$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-prebuilt/releases | jq -r ".[0].tag_name")
@@ -10,11 +7,8 @@ FF=v$(curl -s https://api.github.com/repos/nwjs-ffmpeg-prebuilt/nwjs-ffmpeg-preb
 NW=${NW:1}
 FF=${FF:1}
 
-# If NW.js version is greater than FFmpeg version, exit with error
-FLAG=$(echo "$NW > $FF" | bc -l)
-
-if [ $FLAG = 0 ]; then
-    exit 1;
+if [ "$(echo -e "${NW}\n${FF}" | sort -V | head -n1)" == "$NW" ]; then
+    exit 1
 else
-    exit 0;
+    exit 0
 fi
