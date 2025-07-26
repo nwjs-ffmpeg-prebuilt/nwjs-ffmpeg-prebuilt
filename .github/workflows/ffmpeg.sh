e220a083902de53091dbd66c10a2ae93638869ec
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-07-27 04:01:04 +0530

    Move ffmpeg logic to .sh

---
+++ .github/workflows/ffmpeg.sh
@@ -0,0 +1,7 @@
+FFMPEG_VERSION=$(strings release/linux/build/out/libffmpeg.so | grep -Eo 'ffmpeg version [^ ]+' | head -n 1)
+echo "FFMPEG_VERSION=${FFMPEG_VERSION}" >> $GITHUB_ENV
+echo "## Release Notes" > release-notes.md
+echo "- NW.js version: $NW" >> release-notes.md
+echo "- Chromium version: $(curl -s https://nwjs.io/versions.json | jq -r .versions[0].components.chromium)" >> release-notes.md
+echo "- ${FFMPEG_VERSION}" >> release-notes.md
+echo "- Includes builds for Windows, macOS, and Linux" >> release-notes.md
